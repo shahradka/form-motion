1f326b623db0d51b31a6ab8c4e4bf96ae67e8b14
shahrad kashani <shahrad.kashany@gmail.com>
2025-11-12 12:03:54 +0100

    feat(tailwindcss): ✨ Add Tailwind CSS configuration and setup - global styles for layout

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,       
+      padding: "1rem",    
+      screens: {         
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1400px",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+}
