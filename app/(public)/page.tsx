a2cdce062892928a5c00eac933705bf09f62d630
shahrad kashani <shahrad.kashany@gmail.com>
2025-11-12 17:05:37 +0100

    fix(homePage): 🐛 fix bug homePage in small windows

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -5,8 +5,8 @@ import messageConstants from '@messages';
 import Link from 'next/link';
 export default function Home() {
   return (
-    <div className="w-full flex flex-col items-center grow">
-      <AppCard>
+    <div className="flex flex-col items-center grow w-full">
+      <AppCard className="w-full">
         <div className="p-15">
           <Link href="/counseling">
             <Button size="xl">{`${messageConstants.enter} ${messageConstants.appointment} ${messageConstants.consoling}`}</Button>
--- components/app-card/index.tsx
+++ components/app-card/index.tsx
@@ -1,9 +1,23 @@
 import Image from 'next/image';
 import { Card } from '../ui/card';
+import { AllHTMLAttributes } from 'react';
+import { cn } from '@/lib/utils';
 
-export const AppCard = ({ children }: { children?: React.ReactNode }) => {
+export const AppCard = ({
+  children,
+  className,
+  ...restProps
+}: {
+  children?: React.ReactNode;
+} & AllHTMLAttributes<HTMLDivElement>) => {
   return (
-    <Card className="flex flex-col rounded-b-none border-0! pb-0! overflow-hidden grow bg-primary-foreground items-center justify-end py-2 lg:w-10/12 md:w-10/12">
+    <Card
+      className={cn(
+        'flex flex-col rounded-b-none border-0! pb-0! overflow-hidden grow bg-primary-foreground items-center justify-end py-2 lg:w-10/12 md:w-10/12',
+        className,
+      )}
+      {...restProps}
+    >
       {children}
       <div className="relative w-full h-65">
         <Image src="/road.webp" className="w-full" fill alt="road_img"></Image>
