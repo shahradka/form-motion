b0517df0d3686f36cdf25e5bb0edd1e22af93635
shahrad kashani <shahrad.kashany@gmail.com>
2025-11-12 16:51:19 +0100

    feat(alias): ✨ Add aliasing feature for Message

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,18 @@
 module.exports = {
-  content: [
-    "./app/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
-  ],
+  content: ['./components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
   theme: {
     container: {
-      center: true,       
-      padding: "1rem",    
-      screens: {         
-        sm: "640px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1280px",
-        "2xl": "1400px",
+      center: true,
+      padding: '1rem',
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1400px',
       },
     },
     extend: {},
   },
   plugins: [],
-}
+};
