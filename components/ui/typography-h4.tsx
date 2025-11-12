df54026a65e41a9a442f892997f0cb65fe643805
shahrad kashani <shahrad.kashany@gmail.com>
2025-11-12 12:04:55 +0100

    feat(Fonts): ✨ Add custom fonts and typography styles - add typography components - add new iransans fonts

---
+++ components/ui/typography-h4.tsx
@@ -0,0 +1,5 @@
+export function TypographyH4({ children }: { children?: React.ReactNode }) {
+  return (
+    <h4 className="scroll-m-20 text-xl font-semibold tracking-tight text-foreground">{children}</h4>
+  );
+}
